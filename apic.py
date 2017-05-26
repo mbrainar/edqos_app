a74490a023f6952bc46d8f846ff4d4cefa45d583
Matthew Brainard <mbrainar@cisco.com>
2017-05-26 11:53:08 -0400

    Remove some legacy functions Comment the hell out of it Check that target relevance is valid

--- apic.py
+++ apic.py
@@ -8,87 +8,138 @@
     such as weather events, power excursions, etc.
 '''
 
-# To Do's:
-    # todo convert policy into class
-    # todo add method to modify policy
-    # todo convert REST API calls to use uniq? https://pypi.python.org/pypi/uniq/1.4.0.36
 
-
-
-__author__ = 'sluzynsk'
+__author__ = 'mbrainar'
 
 import requests
-import os
-import json
 from login import login
 
 
-# Define global variables
-if os.environ.get("APIC_SERVER"):
-    apic=os.environ.get("APIC_SERVER")
-else:
-    apic='sandboxapic.cisco.com'
-
-if os.environ.get("APIC_USERNAME"):
-    username=os.environ.get("APIC_USERNAME")
-else:
-    username='devnetuser'
-
-if os.environ.get("APIC_PASSWORD"):
-    password=os.environ.get("APIC_PASSWORD")
-else:
-    password='Cisco123!'
 
 # Create APIC EM object
-client = login()
+# client = login()
+
+
 
 # Create Applications object
 class Applications(object):
-    """
-    Given APIC EM session, create Applications object
-    """
+
     def __init__(self, client):
+        """
+            Initializes the Applications object
+
+            Args:
+                 client: nbClientManager, uniq model
+        """
         self.client = client
+
+
 
     @property
     def applications(self):
+        """
+            Create ApplicationsListResult object
+
+            Returns:
+                 ApplicationsListResult, uniq model
+        """
         return self.client.application.getFilterApplication()
 
+
+
     def get_id(self, app_name):
+        """
+            Given an application name, returns the id of that application
+
+            Args:
+                app_name: name of application
+
+            Returns:
+                 applicationId, string
+        """
         _id = []
         apps = [app for app in self.applications.response if app.name == app_name]
         if len(apps) > 0:
             return apps[0].id
 
-# Create Policy object
-class Policy(object):
-    """
-    Given an APIC EM session, create a Policy object
-    """
-    def __init__(self, client):
-        self.client = client
 
-    # Create PolicyTags object
+
+
+
+class Policy(object):
+
+    def __init__(self, client, policy_scope):
+        """
+            Initializes Policy object
+
+            Args:
+                client: nbClientManager, uniq model
+                policy_scope: policy tag/scope, string
+        """
+        self.client = client
+        self.policy_scope = policy_scope
+
+
+
     @property
     def policy_tags(self):
+        """
+            Creates policy tags list object using policy scope in self
+
+            Returns:
+                PolicyTagResult, uniq model
+        """
         return self.client.policy.getPolicyTags()
 
-    # Create the PolicyListResult object based on the given policy scope
-    def policy_list_for_scope(self, policy_scope):
-        return self.client.policy.getFilterPolicies(policyScope=policy_scope)
 
-    # Get the relevanceLevel for a given app within a given policy scope
-    def app_relevance(self, policy_scope, app_name):
-        for p in self.policy_list_for_scope(policy_scope).response:
+
+    @property
+    def policy_list(self):
+        """
+            Creates policy lists object using policy scope in self
+
+            Returns:
+                 PolicyListResults, uniq model
+        """
+        return self.client.policy.getFilterPolicies(policyScope=self.policy_scope)
+
+
+
+    def app_relevance(self, app_name):
+        """
+            Given an application name, get that applications relevance level
+
+            Args:
+                app_name: application name, string
+
+            Returns:
+                relevanceLevel, string
+        """
+        for p in self.policy_list.response:
             apps = [app for app in p.resource.applications if app.appName == app_name]
             if len(apps) > 0:
                 print("Found application {} in {} policy".format(app_name, p.actionProperty.relevanceLevel))
                 return p.actionProperty.relevanceLevel
 
-    # Given app name and relevance level, reset relevance level
-    def reset_relevance(self, policy_scope, app_name, target_relevance):
+
+
+    def reset_relevance(self, app_name, target_relevance):
+        """
+            Resets the relevance level for an application to the target level for the Policy object self
+
+            Args:
+                app_name: application name, string
+                target_relevance: desired relevance level, string ["Business Relevant", "Default", "Business Irrelevant"]
+        """
+        # Check that target relevance is valid
+        valid_relevance = ["Business Relevant", "Default", "Business Irrelevant"]
+        if target_relevance not in valid_relevance:
+            raise valueError("Invalid relevance")
+
+        # todo get applicationDTO object prior to making changes
+
         _app = []
-        for p in self.policy_list_for_scope(policy_scope).response:
+        for p in self.policy_list.response:
             apps = [app for app in p.resource.applications if app.appName == app_name]
             if len(apps) > 0:
                 if p.actionProperty.relevanceLevel == target_relevance:
@@ -107,58 +158,23 @@ class Policy(object):
                 else:
                     print("Skipping {} policy".format(p.actionProperty.relevanceLevel))
 
-    # PUT the new policy
+
+
     def update_apic(self):
-        self.client.policy.update(policyList=self.policy_list.response)
+        """
+            Updates the policy list in APIC EM through PUT request.
+            Must execute reset_relevance to make changes to object first
+
+            Returns:
+                taskIdResult, uniq model
+        """
+        return self.client.policy.update(policyList=self.policy_list.response)
 
 
-# Get the service ticket to be used in API calls
-def get_ticket():
 
-    reqUrl = "https://{0}/api/v1/ticket".format(apic)
-    payload = {'username': username, 'password': password}
 
-    r = requests.post(reqUrl, json=payload, verify=False)
-
-    if (r.status_code == 200):
-        return r.json()[u'response'][u'serviceTicket']
-    else:
-        r.raise_for_status()
-
-# Get the application ID
-def get_app_id(service_ticket, app_name):
-    reqUrl = "https://{0}/api/v1/application?name={1}".format(apic, app_name)
-    header = {"X-Auth-Token": service_ticket}
-
-    r = requests.get(reqUrl, headers=header, verify=False)
-
-    if (r.status_code == 200):
-        return r.json()['response'][0]['id']
-    else:
-        r.raise_for_status()
-
-# Get the current policy
-def get_policy(service_ticket, policy_scope):
-    reqUrl = "https://{0}/api/v1/policy?policyScope={1}".format(apic, policy_scope)
-    header = {"X-Auth-Token": service_ticket}
-
-    r = requests.get(reqUrl, headers=header, verify=False)
-
-    if r.status_code == 200:
-        return r.json()
-    else:
-        r.raise_for_status()
-
-# Check whether the application is business relevant, irrelevant or default
-def get_app_state(policy, app_id, app_name):
-    for item in policy['response']:
-        for apps in item['resource']['applications']:
-            if app_name in apps.values():
-                return item['actionProperty']['relevanceLevel']
-            else:
-                continue
-
-# Rewrites the policy based on the external event status; if no change is needed, returns false
+# LEGACY Rewrites the policy based on the external event status; if no change is needed, returns false
+"""
 def update_app_state(service_ticket, event_status, policy, app_list):
     if event_status == True:
         print ("Event trigger ON")
@@ -251,36 +267,4 @@ def get_task(service_ticket, task_id):
         return r.json()
     else:
         r.raise_for_status()
-
-# Call API to get list of policy scopes
-# This is used to populate the UI
-def get_policy_scope():
-    policy_tags_object = client.getPolicyTags().response
-    policy_tags_list = []
-    for tag in policy_tags_object.PolicyTag:
-        policy_tags_list.append(tag)
-    return policy_tags_list
-
-
-# Get list of all currently business irrelevant applications
-# This is used to populate the UI
-def get_applications(service_ticket, policy_scope):
-    reqUrl = "https://{0}/api/v1/policy?policyScope={1}".format(apic, policy_scope)
-    header = {"X-Auth-Token": service_ticket}
-
-    r = requests.get(reqUrl, headers=header, verify=False)
-
-    if r.status_code == 200:
-        list = []
-        for policy in r.json()['response']:
-            if policy['actionProperty']['relevanceLevel'] == "Business-Irrelevant":
-                for app in policy['resource']['applications']:
-                    list.append(app['appName'])
-            else:
-                continue
-            list.sort(key=lambda y: y.lower())
-            break
-        return list
-    else:
-        r.raise_for_status()
-
+"""
